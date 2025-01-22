303dadd9dca6502d25f7419f851eb03cd273a715
Moel <mohamedalkasaby1@gmail.com>
2025-01-22 12:54:34 +0100

    edited - posting date

--- client/src/Pages/single.jsx
+++ client/src/Pages/single.jsx
@@ -66,7 +66,7 @@ const single = () => {
           {post.uPhoto && <img className='avatar' src="../public/avatar.png" alt="" />}
           <div>
             <h3>{post.name}</h3>
-            <p>posted {post.postingdate}</p>
+            <p>posted on {post.postingdate}</p>
             </div>
           
           {currentUser!== null && currentUser.name === post.name && (
