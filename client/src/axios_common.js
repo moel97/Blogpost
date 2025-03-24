05f08e3031fc116c07a25f89107aca9d1d43604d
Moel <mohamedalkasaby1@gmail.com>
2025-03-24 20:06:54 +0100

    connecting the new backend

--- client/src/axios_common.js
+++ client/src/axios_common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set a base URL
 const API = axios.create({
-  baseURL: 'https://ec2-54-210-77-134.compute-1.amazonaws.com/api/', // Replace with your base URL
+  baseURL: 'https://blogsphereapi.elkassabi.eu/api/', // Replace with your base URL
 });
 
 export default API;
